2d6f06a8dfa1cb77558e5d8429e62e82b7ef4ed5
tony <tony@1113comp>
2018-04-24 16:16:44 +0700

    должно работать

--- ObjectJ>AlexeyConverter/src/ru/nsc/bionet/fileOperator/fileOperator.java
+++ ObjectJ>AlexeyConverter/src/ru/nsc/bionet/fileOperator/fileOperator.java
@@ -3,6 +3,7 @@ package ru.nsc.bionet.fileOperator;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +13,28 @@ public class fileOperator {
 		BufferedReader reader;
 		List<String> tempLinesList = new ArrayList<String>();
 		try {
-		reader = new BufferedReader(new FileReader(fileName));
-		String line = "";
-		while ((line = reader.readLine()) != null) {
-		    tempLinesList.add(line);
+			reader = new BufferedReader(new FileReader(fileName));
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				tempLinesList.add(line);
 			}
 		}
 		catch (IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
 		}
 		return tempLinesList;
+	}
+
+	public static void saveResults(List<String> results, String filePath, String resultsFileName) throws IOException {
+		PrintStream out;
+		try {
+			out = new PrintStream(filePath + resultsFileName);
+			for (String string : results) {
+				out.println(string);
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- ObjectJ>AlexeyConverter/src/ru/nsc/bionet/objectjToAlexeyConverter/Main.java
+++ ObjectJ>AlexeyConverter/src/ru/nsc/bionet/objectjToAlexeyConverter/Main.java
@@ -7,101 +7,97 @@ import java.util.StringTokenizer;
 import ru.nsc.bionet.fileOperator.fileOperator;
 
 public class Main {
-	static String fileName = "Results_Ulyana.txt"; //имя файла
+	static String filePath = ""; //путь к файлу
+	static String fileName = "Results_Ulyana.txt"; 						//имя файла
 
 	public static void main(String[] args) {
 		/*
 		 * загружаем из файла
-		 */
-		List<String> tempList = new ArrayList<String>(); //список для загрузки из файла
-		tempList = fileOperator.readFile(fileName); //загружаем
-		
+		 * @todo переименовать fileOperator -> LeafFileRW
+		 */ 
+		List<String> tempList = new ArrayList<String>(); 				//список для загрузки из файла
+		tempList = fileOperator.readFile(fileName); 					//загружаем
+
 		/*
 		 * обрабатываем заголовок
 		 */
-		String headerIn = tempList.get(0); //берем строку с заголовком
-		tempList.remove(0); //удаляем строку с заголовком чтобы больше не мешалась
-		
+		String headerIn = tempList.get(0); 								//берем строку с заголовком
+		tempList.remove(0); 											//удаляем строку с заголовком чтобы больше не мешалась
 
-		StringTokenizer headerTokenized = new StringTokenizer(headerIn,"\t"); //разбираем на токены
-		String headerOut = "";	//заголовок, который будет выведен в файл
+		StringTokenizer headerTokenized = new StringTokenizer(headerIn,"\t"); 	//разбираем на токены
+		String headerOut = "";													//заголовок, который будет выведен в файл
 		int headerIndex = headerTokenized.countTokens();
-		int LeafLengthIndex = 0;
-		int MarkerIndex = 0;
-		int FileNameIndex = 0;
-		while (headerTokenized.hasMoreTokens()){				//проходим по токенам, запоминаем индекс заголовков
+		int leafLengthIndex = 0;
+		int markerIndex = 0;
+		int fileNameIndex = 0;
+		while (headerTokenized.hasMoreTokens()){								//проходим по токенам, запоминаем индекс заголовков
 			String headerPart = headerTokenized.nextToken();
 			switch (headerPart) {
 			case "LeafLength":
-				LeafLengthIndex = headerIndex - headerTokenized.countTokens() - 1;
+				leafLengthIndex = headerIndex - headerTokenized.countTokens() - 1;
 				break;
 			case "Marker":
 				headerOut = headerOut + headerPart + "\t";
-				MarkerIndex = headerIndex - headerTokenized.countTokens() - 1;
+				markerIndex = headerIndex - headerTokenized.countTokens() - 1;
 				break;
 			case "FileName":
 				headerOut = headerOut + headerPart + "\t";
-				FileNameIndex = headerIndex - headerTokenized.countTokens() - 1;
+				fileNameIndex = headerIndex - headerTokenized.countTokens() - 1;
 				break;
 			default:
 				break;
 			}
 		}
-		headerOut = headerOut + "Leaf_1\t" + "Leaf_2\t" + "Leaf_3\t" + "Leaf_4\t" + "Leaf_5";
+		headerOut = headerOut + "Leaf_1\t" + "Leaf_2\t" + "Leaf_3\t" + "Leaf_4\t" + "Leaf_5"; //собираем заголовок, добавляем столбцы с листьями
 		System.out.println(headerOut);
-		System.out.println(LeafLengthIndex + " " + MarkerIndex + "\t" + FileNameIndex);
-		
+		System.out.println(leafLengthIndex + " " + markerIndex + "\t" + fileNameIndex);
+
 		/*
 		 * обрабатываем данные
 		 */
-		
-		
-		while (tempList.isEmpty() != true) { //пока в списке есть строки работаем
-			String lineIn = tempList.get(0); //берем первую строку
-			tempList.remove(0); //удаляем эту строку чтобы больше не мешалась
-			
-			StringTokenizer lineInTokenized = new StringTokenizer(lineIn,"\t"); //разбираем на токены
-			int lineIndex = lineInTokenized.countTokens(); //размер строки
-			String[] lineOutArray = new String[lineIndex];	//массив строк под 1 растение, которые будут выведены в файл
-			int leafIndex = 0; //номер листа у растения
-			
-			while (lineInTokenized.hasMoreTokens()) { //
+
+		int markerIndexPrevious = 0; //стартовое значение счетчика маркеров
+		String lineOut = "";//выходные данные, текущая строка
+		List<String> plantsList = new ArrayList<String>(); //выходные данные, построчно в список
+
+		while (tempList.isEmpty() != true) { //пока во входном списке есть строки работаем
+			/*
+			 * формируем блок данных по всем листьям одного растения
+			 */		
+			String lineIn = tempList.get(0); //берем очередную строку из начала списка
+			tempList.remove(0); //удаляем эту строку из массива, чтобы больше не мешалась
+
+			StringTokenizer lineInTokenized = new StringTokenizer(lineIn,"\t"); //разбираем строку на токены
+
+			int lineIndex = lineInTokenized.countTokens(); //определяем размер массива для перевода токенизированной строки в массив входных данных 
+			String[] lineInArray = new String[lineIndex];	//создаем массив входных данных
+
+			while (lineInTokenized.hasMoreTokens()) { //переводим токены в массив входных данных
 				String linePart = lineInTokenized.nextToken();
-				switch (lineIndex - lineInTokenized.countTokens() - 1) { // смотрим что это - лист, имя файла, маркер растения...
-				case LeafLengthIndex:
-					System.out.println(linePart);
-					break;
-				case MarkerIndex: // если это маркер, то смотрим 
-					System.out.println(linePart);
-					if (lineOutArray[MarkerIndex] == linePart + "\t");
-					lineOutArray[MarkerIndex] = linePart + "\t";
-					break;
-				case FileNameIndex: // тупо переписываем имя файла даже если оно такое же
-					System.out.println(linePart);
-					lineOutArray[FileNameIndex] = linePart + "\t";
-					break;
-				default:
-					break;
+				lineInArray[lineIndex - lineInTokenized.countTokens() - 1] = linePart;
 			}
-			String[] lineInArray = 
-			while (headerTokenized.hasMoreTokens()){				//проходим по токенам строки
-				String headerPart = headerTokenized.nextToken();
-				/*
-				 * выделяем блок
-				 */
-				int headerIndex = headerTokenized.countTokens();
-				int LeafLengthIndex = 0;
-				int MarkerIndex = 0;
-				int FileNameIndex = 0;
-				MarkerIndex = headerIndex - headerTokenized.countTokens() - 1;
-				
+
+			//пересобираем входную строку в заданном порядке {маркер растения, имя файла, длина листа 1, ...}
+			if (Integer.parseInt(lineInArray[markerIndex]) == markerIndexPrevious) { //если предыдущая строка относится к тому же растению продолжаем дописывать длины листьев
+				lineOut = lineOut + lineInArray[leafLengthIndex] + "\t"; //дописываем длину следующего листа к выводу
+				markerIndexPrevious = Integer.parseInt(lineInArray[markerIndex]); //ставим текущий маркер растения
+			} else {//if (markerIndex !== markerIndexPrevious) //если перешли к следующему маркеру или только начали (первый маркер 1)
+				//скидываем готовую выводную строку в массив, подрезаем на всякий
+				plantsList.add(lineOut.trim());
+				//обнуляем выводную строку под следующее растение
+				lineOut = "";
+				lineOut = lineInArray[markerIndex] + "\t"; //записываем маркер и имя файла
+				lineOut = lineOut + lineInArray[fileNameIndex] + "\t";
+				lineOut = lineOut + lineInArray[leafLengthIndex] + "\t";//записываем длину 1 листа
+				markerIndexPrevious = markerIndex;
+			}	
 		}
+
 		/*
-		 * трансформируем в нужный формат блока
+		 * вывод в файл
 		 */
-		
-		/*
-		 * дописываем в вывод
-		 */
+		fileOperator.saveResults(plantsList, filePath, fileName);
 	}
+}
+
 }
